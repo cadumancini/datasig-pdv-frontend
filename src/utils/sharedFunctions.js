0db0d68f01b95e418e8a7cb3cd87577d08c6fce3
Carlos Mancini <carlos@devsquad.email>
2024-04-26 14:44:25 -0300

    Melhoria para msgs de erro

--- src/utils/sharedFunctions.js
+++ src/utils/sharedFunctions.js
@@ -5,6 +5,8 @@ var functions = {
         alert('Seu token de acesso não é mais válido. Por favor, faça login novamente.')
         sessionStorage.clear()
         location.reload()
+      } else {
+        if(err.response.data.message) alert (err.response.data.message)
       }
     }
   }
