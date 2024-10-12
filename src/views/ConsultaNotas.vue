d8ede885edb5c4af40f765d87069ffde8152e8aa
Carlos Mancini <carlos@devsquad.email>
2024-10-12 16:37:13 -0300

    Melhoria de nome de classe CSS

--- src/views/ConsultaNotas.vue
+++ src/views/ConsultaNotas.vue
@@ -83,7 +83,7 @@
       </div>
       <div class="row table-registros border">
         <table class="table table-striped table-hover table-sm table-responsive table-items">
-          <thead class="header-notas">
+          <thead class="header-fixed">
             <tr>
               <th class="sm-header" style="width: 5%;"><small>Empresa</small></th>
               <th class="sm-header" style="width: 4%;"><small>Filial</small></th>
